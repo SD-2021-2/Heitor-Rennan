c45ee4e49abc88368e4be2c6854db86d3a402de0
Rennan <rennan11@github.com>
2021-08-21 12:58:44 -0300

    fix type of socket connection

--- Lista 2/ex3/ex3.py
+++ Lista 2/ex3/ex3.py
@@ -3,7 +3,7 @@ import json
 
 HOST = '127.0.0.1'  # Endereco IP do Servidor
 PORT = 6565            # Porta que o Servidor esta
-skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 skt.connect(dest)
 
