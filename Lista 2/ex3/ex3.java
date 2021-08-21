d1da31c05f64e681c5b9744fc3920ee252d6c161
Heitor Vieira Mello <heitor29_@hotmail.com>
2021-08-21 11:58:51 -0300

    ex3 e ex4

---
+++ Lista 2/ex3/ex3.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ex3 {
+	public static void main(String[] args) throws IOException {
+		
+            ServerSocket serverSocket = new ServerSocket(6565);
+            System.out.println("Waiting connections");
+            Socket socket;
+		
+		while(true){
+			socket = null;
+			socket = serverSocket.accept(); 
+			System.out.println("Conexao realizada!");
+			
+			new Ex3(socket).start();
+		}		
+    }
+}
+
+class Ex3 extends Thread {
+
+	Socket thread;
+  
+	Ex3 (Socket socket) throws IOException {
+		thread = socket;
+	}
+  
+	public void run() throws JSONException {
+		try {
+		InputStream inputStream = thread.getInputStream();
+                BufferedReader mensagem = new BufferedReader(new InputStreamReader (inputStream));
+		JSONObject json_obj = new JSONObject(mensagem.readLine());
+
+		NotasParaMedia notas = new NotasParaMedia();
+
+		notas.nota1 = json_obj.getDouble("n1");
+		notas.nota2 = json_obj.getDouble("n2");
+		notas.nota3 = json_obj.getDouble("n3");
+		
+		json_obj.put("resultado", notas.aprovado());
+
+		PrintStream resposta = new PrintStream(thread.getOutputStream());
+		resposta.println(json_obj.toString());
+
+		thread.close();
+                
+		System.out.println("Conexao finalizada!");
+				
+		}catch (IOException e) {
+			System.out.println("Conexao com problemas!");
+		}
+	}  
+}
+
+class NotasParaMedia {
+	double nota1, nota2, nota3;
+	
+	String aprovado(){
+		
+		double media = (nota1+nota2)/2;
+		if(media >= 7.0 || (media > 3.0 && (media+nota3)/2 >= 5.0))
+			return "Aprovado!";
+		else
+			return "Reprovado!";
+		
+        }
+}
---
+++ Lista 2/ex4/ex4.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ex4 {
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(6565);
+		Socket socket;
+                
+                System.out.println("Waiting Connections");
+		
+		while(true){
+			socket = null;
+			socket = serverSocket.accept(); 
+			System.out.println("Conexao realizada!");
+			
+			new Ex4(socket).start();
+		}		
+    }
+}
+
+class Ex4 extends Thread {
+
+	Socket thread;
+  
+	Ex4 (Socket socket) throws IOException {
+		thread = socket;
+	}
+  
+        @Override
+	public void run() throws JSONException {
+            try {
+		InputStream inputStream = thread.getInputStream();
+                BufferedReader mensagem = new BufferedReader(new InputStreamReader (inputStream));
+	
+		JSONObject json_obj = new JSONObject(mensagem.readLine());
+
+		Pessoa pessoa = new Pessoa();
+
+		pessoa.height = json_obj.getDouble("altura");
+		pessoa.gender = json_obj.getString("sexo");
+		
+		json_obj.put("peso", pessoa.pesoParaSaude());
+
+		PrintStream resposta = new PrintStream(thread.getOutputStream());
+		resposta.println(json_obj.toString());
+
+		thread.close();
+		System.out.println("Conexao finalizada!");
+		
+            }catch (IOException e) {
+			System.out.println("Erro na conexao!");
+            }
+	}  
+}
+
+class Pessoa {
+	double height;
+	String gender;
+	
+	String pesoParaSaude(){
+		
+		double peso;
+		if(gender.equals("Masculino")){
+			peso = (72.7*height)-58;
+			return String.format("Seu peso ideal: %.2f", peso);
+		}else if(gender.equals("Feminino")){
+			peso = (62.1*height)-44.7;
+			return String.format("Seu peso ideal: %.2f", peso);
+		} else {
+                    return "Sexo inexistente";
+                }
+		
+	}
+}
