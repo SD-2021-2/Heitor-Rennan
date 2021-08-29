4378d7e74fb517f1b8ea621cabb7e9fbae085c53
Heitor Vieira Mello <heitor29_@hotmail.com>
2021-08-29 18:21:47 -0300

    Criacao Pastas Client e Server

---
+++ Lista3/Client/ex1.py
@@ -0,0 +1,22 @@
+import socket
+import json
+
+HOST = '127.0.0.1'  # Endereco IP do Servidor
+PORT = 6565            # Porta que o Servidor esta
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+skt.connect(dest)
+
+nome = input('Informe nome do funcionario\n')
+
+obj = {
+    "nome": nome
+}
+
+msg = json.dumps(obj)
+
+skt.sendall((msg+"\n").encode())
+
+res = skt.recv(1024)
+data = json.loads(res.decode())
+print(data["reajuste"])
